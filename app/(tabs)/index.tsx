9c25848c80e9bb805c2c16eed7f122950a674e01
FelipeOPratinha <felipeaires98@hotmail.com>
2025-11-25 21:23:29 -0300

    [MOBILE] Criação da tela principal do aplicativo

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,101 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+import { useAuthGuard } from "@/auth/useAuthGuard";
+import { Ionicons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+import { useRouter } from "expo-router";
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useAuth } from "@/auth/useAuth";
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+  useAuthGuard();
+  const router = useRouter();
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+  const { user } = useAuth();
+
+  return (
+    <View style={styles.container}>
+      <LinearGradient
+        colors={["#5E60CE", "#4EA8DE"]}
+        style={styles.header}
+      >
+        <Text style={styles.headerTitle}>Me Chame Já</Text>
+        <Text style={styles.headerSubtitle}>Notificações em tempo real</Text>
+      </LinearGradient>
+
+      <ScrollView contentContainerStyle={styles.content}>
+        {/* GOSTARIA DE COLOCAR AQUI */}
+        {user && (
+          <TouchableOpacity style={styles.card}>
+            <Ionicons name="person-circle-outline" size={40} color="#64DFDF" />
+            <View style={{ flex: 1 }}>
+              <Text style={styles.cardTitle}>Olá, {user.name}</Text>
+              <Text style={styles.cardDesc}>É um prazer ter você de volta!</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+
+        {/* MINHAS ENTREGAS */}
+        <TouchableOpacity
+          style={styles.card}
+          onPress={() => router.push("/dashboard")}
+        >
+          <Ionicons name="bar-chart" size={40} color="#64DFDF" />
+          <View style={{ flex: 1 }}>
+            <Text style={styles.cardTitle}>Dashboard</Text>
+            <Text style={styles.cardDesc}>Veja dados da sua atividade</Text>
+          </View>
+          <Ionicons name="chevron-forward" size={24} color="#FFF" />
+        </TouchableOpacity>
+
+        {/* MINHAS ENTREGAS */}
+        <TouchableOpacity
+          style={styles.card}
+          onPress={() => router.push("/minhas-entregas")}
+        >
+          <Ionicons name="list" size={40} color="#64DFDF" />
+          <View style={{ flex: 1 }}>
+            <Text style={styles.cardTitle}>Minhas Entregas</Text>
+            <Text style={styles.cardDesc}>Veja o status atualizado</Text>
+          </View>
+          <Ionicons name="chevron-forward" size={24} color="#FFF" />
+        </TouchableOpacity>
+
+        {/* NOTIFICAÇÕES */}
+        <TouchableOpacity
+          style={styles.card}
+          onPress={() => router.push("/notificacoes")}
+        >
+          <Ionicons name="notifications" size={40} color="#64DFDF" />
+          <View style={{ flex: 1 }}>
+            <Text style={styles.cardTitle}>Notificações</Text>
+            <Text style={styles.cardDesc}>Atualizações das suas entregas</Text>
+          </View>
+          <Ionicons name="chevron-forward" size={24} color="#FFF" />
+        </TouchableOpacity>
+
+        {/* CONFIGURAÇÕES */}
+        <TouchableOpacity
+          style={styles.card}
+          onPress={() => router.push("/configuracoes")}
+        >
+          <Ionicons name="settings" size={40} color="#64DFDF" />
+          <View style={{ flex: 1 }}>
+            <Text style={styles.cardTitle}>Configurações</Text>
+            <Text style={styles.cardDesc}>Preferências e conta</Text>
+          </View>
+          <Ionicons name="chevron-forward" size={24} color="#FFF" />
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+  container: { flex: 1, backgroundColor: "#000000ff" },
+  header: { paddingTop: 70, paddingBottom: 40, paddingHorizontal: 24, borderBottomLeftRadius: 30, borderBottomRightRadius: 30, },
+  headerTitle: { color: "#FFF", fontSize: 32, fontWeight: "bold" },
+  headerSubtitle: { color: "#E8E8E8", fontSize: 16, marginTop: 4 },
+  content: { padding: 20, gap: 16 },
+  card: { flexDirection: "row", alignItems: "center", backgroundColor: "#2C2C2E", padding: 20, borderRadius: 20, gap: 20, },
+  cardTitle: { color: "#FFF", fontSize: 18, fontWeight: "600" },
+  cardDesc: { color: "#AAA", fontSize: 14 },
 });
