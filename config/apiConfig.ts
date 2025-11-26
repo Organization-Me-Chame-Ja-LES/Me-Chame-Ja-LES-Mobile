38a52e5260dcbeb4d1a93197b0a043dd24f949aa
FelipeOPratinha <felipeaires98@hotmail.com>
2025-11-25 21:34:10 -0300

    [MOBILE] Arquivo de configuração de IP de máquina, apenas para desenvolvimento

---
+++ config/apiConfig.ts
@@ -0,0 +1,8 @@
+// IP do PC rodando o backend
+export const BACKEND_IP = "192.168.0.17";
+
+// Porta
+export const BACKEND_PORT = "3000";
+
+// URL de conexão
+export const BASE_URL = `http://${BACKEND_IP}:${BACKEND_PORT}`;
